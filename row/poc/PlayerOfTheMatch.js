331c2042fb54ed30428e659d737937160d0d492a
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2021-03-21 15:04:32 +0530

    displayed player of the match serially not by asyn

--- row/poc/PlayerOfTheMatch.js
+++ row/poc/PlayerOfTheMatch.js
@@ -13,24 +13,30 @@ function extractData(html) {
     let selTool = cheerio.load(html);
 
     let batsManTable = selTool(".col-md-8.col-16");
+    let arr_link=[];
     for(let i=0;i<batsManTable.length;i++){
         let cardBtn=selTool(batsManTable[i]).find(".btn.btn-sm.btn-outline-dark.match-cta");
         let link="https://www.espncricinfo.com" +selTool(cardBtn[2]).attr("href");
-        
-        printBirthDay(link)
+        arr_link.push(link);
     }
+    printBirthDay(arr_link,0);
 }
 
-function printBirthDay(link){
-    request(link,cb);
-
-    function cb(err,response,html){
-        if(err){
-            console.log(err);
-        }else{
-            extractBirthDay(html);
+function printBirthDay(arr_link,i){
+    if(arr_link.length==i){
+        return;
+    }else{
+        request(arr_link[i],cb);
+        function cb(err,response,html){
+            if(err){
+                console.log(err);
+            }else{
+                extractBirthDay(html);
+                printBirthDay(arr_link,i+1);
+            }
         }
     }
+
 }
 function extractBirthDay(html){
     let selTool=cheerio.load(html);
